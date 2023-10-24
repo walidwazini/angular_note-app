bcf335d49e3f5f9f64b564a972c7d0d630b1f6cf
Walid Wazini <wazini25@gmail.com>
2023-10-24 17:10:36 +0800

    add card component | dummy data

---
+++ src/data/index.ts
@@ -0,0 +1,25 @@
+
+
+
+export const dummyNotes = [
+  {
+    id:Math.random(),
+    note: 'Learn about Studio.io',
+    date: new Date(2024,4,12),
+  },
+  {
+    id:Math.random(),
+    note: 'Create new project using Ionic-Angular',
+    date: new Date(2024,4,12),
+  },
+  {
+    id:Math.random(),
+    note: 'Go to BankIslam to change new debit card.',
+    date: new Date(2024,4,12),
+  },
+  {
+    id:Math.random(),
+    note: 'Learn about Prisma js (framework for express).',
+    date: new Date(2024,4,22),
+  },
+]
